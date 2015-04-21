7b506fe0945a4522090989785d18df12b1746701
Daniel Flax <dflax@yahoo.com>
2015-04-20 22:33:45 -0400

    Added some properties to the storyboard collection view controller.

---
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+//  Created by Daniel Flax on 4/20/15.
+//  Copyright (c) 2015 Daniel Flax. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UICollectionViewController {
+
+	private let reuseIdentifier = "FlickrCell"
+	private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+
+}
