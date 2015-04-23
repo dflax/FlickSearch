8478a58c514ab782bd49e86d378ca3e14aa7418a
Daniel Flax <dflax@yahoo.com>
2015-04-23 13:44:46 -0400

    Selection logic.

--- FlickrSearch/FlickrPhotosViewController.swift
+++ FlickrSearch/FlickrPhotosViewController.swift
@@ -11,11 +11,42 @@ import UIKit
 class FlickrPhotosViewController: UICollectionViewController {
 
 	private let reuseIdentifier = "FlickrCell"
-	private let sectionInsets = UIEdgeInsets(top: 25.0, left: 10.0, bottom: 25.0, right: 10.0)
+	private let sectionInsets = UIEdgeInsets(top: 15.0, left: 10.0, bottom: 15.0, right: 10.0)
 
 	// Data structures
 	private var searches = [FlickrSearchResults]()
 	private let flickr = Flickr()
+
+	// To track whether something is selected
+	//1
+	var largePhotoIndexPath : NSIndexPath? {
+		didSet {
+			
+			//2
+			var indexPaths = [NSIndexPath] ()
+			if largePhotoIndexPath != nil {
+				indexPaths.append(largePhotoIndexPath!)
+			}
+			if oldValue != nil {
+				indexPaths.append(oldValue!)
+			}
+			
+			//3
+			collectionView?.performBatchUpdates( {
+				self.collectionView?.reloadItemsAtIndexPaths(indexPaths)
+				return
+				} )
+				{
+					completed in
+					
+					//4
+					if self.largePhotoIndexPath != nil {
+						self.collectionView?.scrollToItemAtIndexPath(
+							self.largePhotoIndexPath!, atScrollPosition: .CenteredVertically, animated: true)
+					}
+				}
+		}
+	}
 
 	func photoForIndexPath(indexPath: NSIndexPath) -> FlickrPhoto {
 		return searches[indexPath.section].searchResults[indexPath.row]
@@ -74,36 +105,82 @@ extension FlickrPhotosViewController : UICollectionViewDataSource {
 
 	override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
 
-		//1
 		let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! FlickrPhotoCell
+		let flickrPhoto = photoForIndexPath(indexPath)
+
+		//1
+		cell.activityIndicator.stopAnimating()
 
 		//2
-		let flickrPhoto = photoForIndexPath(indexPath)
-		cell.backgroundColor = UIColor.blackColor()
+		if indexPath != largePhotoIndexPath {
+			cell.imageView.image = flickrPhoto.thumbnail
+			return cell
+		}
 
 		//3
+		if flickrPhoto.largeImage != nil {
+			cell.imageView.image = flickrPhoto.largeImage
+			return cell
+		}
+
+		//4
 		cell.imageView.image = flickrPhoto.thumbnail
+		cell.activityIndicator.startAnimating()
+
+		//5
+		flickrPhoto.loadLargeImage {
+			loadedFlickrPhoto, error in
+
+			//6
+			cell.activityIndicator.stopAnimating()
+
+			//7
+			if error != nil {
+				return
+			}
+
+			if loadedFlickrPhoto.largeImage == nil {
+				return
+			}
+
+			//8
+			if indexPath == self.largePhotoIndexPath {
+				if let cell = collectionView.cellForItemAtIndexPath(indexPath) as? FlickrPhotoCell {
+					cell.imageView.image = loadedFlickrPhoto.largeImage
+				}
+			}
+		}
 
 		return cell
 	}
+
+
 }
 
 
 // MARK: - UICollectionViewDelegateFlowLayout flow layout
 extension FlickrPhotosViewController : UICollectionViewDelegateFlowLayout {
 
-	//1
 	func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
 
-			let flickrPhoto =  photoForIndexPath(indexPath)
+		let flickrPhoto = photoForIndexPath(indexPath)
 
-			//2
-			if var size = flickrPhoto.thumbnail?.size {
-				size.width  += 10
-				size.height += 10
-				return size
-			}
-			return CGSize(width: 100, height: 100)
+		// New code
+		if indexPath == largePhotoIndexPath {
+			var size = collectionView.bounds.size
+			size.height -= topLayoutGuide.length
+			size.height -= (sectionInsets.top + sectionInsets.right)
+			size.width -= (sectionInsets.left + sectionInsets.right)
+			return flickrPhoto.sizeToFillWidthOfSize(size)
+		}
+
+		// Previous code
+		if var size = flickrPhoto.thumbnail?.size {
+			size.width += 10
+			size.height += 10
+			return size
+		}
+		return CGSize(width: 100, height: 100)
 	}
 
 	//3
@@ -112,5 +189,19 @@ extension FlickrPhotosViewController : UICollectionViewDelegateFlowLayout {
 	}
 
 }
+
+// MARK: - Selection methods
+extension FlickrPhotosViewController : UICollectionViewDelegate {
+
+	override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+		if largePhotoIndexPath == indexPath {
+			largePhotoIndexPath = nil
+		} else {
+			largePhotoIndexPath = indexPath
+		}
+		return false
+	}
+}
+
 
 
